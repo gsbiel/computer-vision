f694a86decbc8b1ada7c8aa4c8542fb297cdb789
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-10-25 23:21:44 -0300

    implement camera rotation around single axes at a time and at any position

--- workdir/camera.py
+++ workdir/camera.py
@@ -16,27 +16,56 @@ class CameraAxis(Object):
         super().transform(transformation_matrix)
         self.p_1 = (transformation_matrix.dot(self.p_1.transpose())).transpose()
 
+    def translateX(self, dx):
+        super().translateX(dx)
+        self.p_0 = self.body
+        return
+
+    def translateY(self, dy):
+        super().translateY(dy)
+        self.p_0 = self.body
+        return
+
+    def translateZ(self, dz):
+        super().translateZ(dz)
+        self.p_0 = self.body
+        return
+
+    def translateXY(self, code, d):
+        super().translateXY(code, d)
+        self.p_0 = self.body
+        return
+
     def rotateX(self, angle):
-        self.body = np.vstack((self.p_1, self.p_0))
-        super().rotateX(angle)
-        self.p_0 = np.array([self.body[3,:]])
-        self.p_1 = np.array([self.body[0,:], self.body[1,:], self.body[2,:]])
+        self.body = self.p_1
+        super().rotateBackToInitialPosition()
+        self.x_orientation = angle
+        rotation_matrix = super().get_rotation(self.z_orientation, angle, self.y_orientation)
+        directionVectorsRotated = rotation_matrix.dot(self.body.transpose())
+        self.p_1 = directionVectorsRotated.transpose()
+        super().trackRotation(rotation_matrix)
         self.body = self.p_0
         return
 
     def rotateY(self, angle):
-        self.body = np.vstack((self.p_1, self.p_0))
-        super().rotateY(angle)
-        self.p_0 = np.array([self.body[3,:]])
-        self.p_1 = np.array([self.body[0,:], self.body[1,:], self.body[2,:]])
+        self.body = self.p_1
+        super().rotateBackToInitialPosition()
+        self.y_orientation = angle
+        rotation_matrix = super().get_rotation(self.z_orientation, self.x_orientation, angle)
+        directionVectorsRotated = rotation_matrix.dot(self.body.transpose())
+        self.p_1 = directionVectorsRotated.transpose()
+        super().trackRotation(rotation_matrix)
         self.body = self.p_0
         return
 
     def rotateZ(self, angle):
-        self.body = np.vstack((self.p_1, self.p_0))
-        super().rotateZ(angle)
-        self.p_0 = np.array([self.body[3,:]])
-        self.p_1 = np.array([self.body[0,:], self.body[1,:], self.body[2,:]])
+        self.body = self.p_1
+        super().rotateBackToInitialPosition()
+        self.z_orientation = angle
+        rotation_matrix = super().get_rotation(angle, self.x_orientation, self.y_orientation)
+        directionVectorsRotated = rotation_matrix.dot(self.body.transpose())
+        self.p_1 = directionVectorsRotated.transpose()
+        super().trackRotation(rotation_matrix)
         self.body = self.p_0
         return
 
